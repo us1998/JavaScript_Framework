f4a2a8b12a5a9afe60f57c0503a96083117f720a
Umang Sawaria <umangsawaria1998@gmail.com>
2023-04-23 22:03:56 +0530

    Update ReadLine.js

--- NodeJS/ReadLine.js
+++ NodeJS/ReadLine.js
@@ -7,9 +7,25 @@ let num2 = Math.floor((Math.random() * 10) + 1);
 let ans = num1 + num2;
 
 // calling readline question method and verifying output and closing the interface
-rl.question(`what is ${num1} + ${num2} ?\n` , (userInput) =>{
+rl.question(`What is ${num1} + ${num2} ?\n` , (userInput) =>{
     if(userInput.trim() == ans){
         rl.close();
+    }
+    else{
+        /*This method is used to output the value set by rl.setPrompt() in the new line in the console.*/
+        rl.setPrompt("Incorrect response Please try again\n");
+        /*The rl. prompt() method writes the readline. Interface instances configured prompt to a new 
+        line in output in order to provide a user with a new location at which to provide input.*/
+        rl.prompt();
+        rl.on('line' , (userInput) => {
+            if(userInput.trim() == ans){
+                rl.close();
+            }
+            else{
+                rl.setPrompt(`Your input of ${userInput} is wrong.Please provide correct response\n`);
+                rl.prompt();
+            }
+        });
     }
 });
 
